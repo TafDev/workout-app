80a3f284154feb4f777ea623d5dd4fa904b3e29c
TafDev <chombot@hotmail.com>
2017-01-06 18:38:52 +0000

    create exercise succefully

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,3 +1,8 @@
 class Exercise < ApplicationRecord
-  belongs_to :user
+	belongs_to :user
+
+	validates :duration_in_min, numericality: {greather_than: 0.0}, presence: true
+	validates :workout, presence: true
+	validates	:workout_date, presence: true
+
 end
